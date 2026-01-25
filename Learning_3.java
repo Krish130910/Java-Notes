1fd93a0c12b702f80827f74f703c7fc948de4db7
Krish <krish.savaliya130910@marwadiuniversity.ac.in>
2026-01-25 18:00:40 +0530

    Random NO and Math CLass

--- Learning_3.java
+++ Learning_3.java
@@ -1,5 +1,5 @@
 // JAVA NOTES
-// if else Statements, Random Number
+// if else Statements, Random Number and Math Class in Java
 
 // In Java, if-else statements are used to perform conditional operations based on whether a certain condition is true or false.
 
@@ -36,3 +36,45 @@
 // In this example, the program checks whether the variable 'number' is positive, negative, or zero and prints the corresponding message.
 
 // Random Number Generation in Java:
+// In Java, you can generate random numbers using the java.util.Random class or the Math.random() method.
+
+// import java.util.Random;
+// public class Learning_3 {
+//     public static void main(String[] args) {
+//        Random random = new Random();
+//        int number;
+//        number = random.nextInt();
+//        System.out.println("Random Number: " + number);
+//     }
+// }
+
+// In this example, we create an instance of the Random class and use the nextInt() method to generate a random integer.
+// You can also specify a range for the random number. For example, to generate a random number between 0 (inclusive) and 100 (exclusive):
+// int randomNumber = random.nextInt(100); // Generates a number between 0 and 99
+// System.out.println("Random Number between 0 and 99: " + randomNumber);
+
+// To generate multiple random numbers, you can assign the random number generation to a variable multiple times.
+// import java.util.Random;
+// public class Learning_3 {        
+//     public static void main(String[] args) {
+//        Random random = new Random();
+//        int number1 = random.nextInt(100); // First random number between 0 and 99
+//        int number2 = random.nextInt(100); // Second random number between 0 and 99
+//        System.out.println("First Random Number: " + number1);
+//        System.out.println("Second Random Number: " + number2);
+//     }
+// }
+// In this example, we generate two different random numbers between 0 and 99 and print them to the console.
+
+// Not only you can generate random integers, but you can also generate random doubles, booleans, and more using the Random class methods like nextDouble(), nextBoolean(), etc.
+// import java.util.Random;
+// public class Learning_3 {
+//     public static void main(String[] args) {
+//        Random random = new Random();
+//        double randomDouble = random.nextDouble(); // Generates a random double between 0.0 and 1.0
+//        boolean randomBoolean = random.nextBoolean(); // Generates a random boolean value (true or false)
+       
+//        System.out.println("Random Double: " + randomDouble);
+//        System.out.println("Random Boolean: " + randomBoolean);
+//     }
+// }
