e65887dbe31d9ad9feb8aad823fd6cd31fefc309
Krish <krish.savaliya130910@marwadiuniversity.ac.in>
2026-01-25 17:50:08 +0530

    if else

---
+++ Learning_3.java
@@ -0,0 +1,38 @@
+// JAVA NOTES
+// if else Statements, Random Number
+
+// In Java, if-else statements are used to perform conditional operations based on whether a certain condition is true or false.
+
+// The syntax for if-else statements is as follows:
+// if (condition) {
+//     // Code to be executed if the condition is true
+// } else {
+//     // Code to be executed if the condition is false
+// }
+
+// You can also use else if to check multiple conditions:
+// if (condition1) {
+//     // Code to be executed if condition1 is true
+// } else if (condition2) {
+//     // Code to be executed if condition2 is true
+// } else {
+//     // Code to be executed if both conditions are false
+// }    
+
+// public class Learning_3 {
+//     public static void main(String[] args) {
+//         int number = 10;
+
+//         // Check if the number is positive, negative, or zero
+//         if (number > 0) {
+//             System.out.println(number + " is a positive number.");
+//         } else if (number < 0) {
+//             System.out.println(number + " is a negative number.");
+//         } else {
+//             System.out.println("The number is zero.");
+//         }
+//     }
+// }
+// In this example, the program checks whether the variable 'number' is positive, negative, or zero and prints the corresponding message.
+
+// Random Number Generation in Java:
