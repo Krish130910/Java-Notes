c5ccbefd6a982019a5ed1b21f59610d02eae1677
Krish <krish.savaliya130910@marwadiuniversity.ac.in>
2026-01-26 15:46:00 +0530

    Weight converter prg and substrings method

---
+++ Learning_6.java
@@ -0,0 +1,50 @@
+// JAVA NOTES
+// Substring Method
+
+// Substring method is used to extract a portion of a string based on specified indices.
+// Syntax for substring method:
+// String.substring(start, end)
+// - start: The starting index (inclusive) from where the substring begins.
+// - end: The ending index (exclusive) where the substring ends.
+// Substring return a new string that contains the characters from the original string starting from the 'start' index up to, but not including, the 'end' index
+
+// Example of using substring method in Java:
+// public class Learning_6 {
+//     public static void main(String[] args) {
+//         String email = "Bro123@gmail.com";
+//         String username = email.substring(0, 6); // Extracts "Bro123"
+//         String UName = email.substring(0, email.indexOf("@")); // Extracts "Bro123"
+//         String DName = email.substring(email.indexOf("@") + 1); // Extracts "gmail.com" 
+//         String Dnamee = email.substring(email.indexOf("@")); // Extracts "@gmail.com"
+//         String domain = email.substring(6); // Extracts "gmail.com" (from index 6 to the end of the string)
+//         System.out.println("Username: " + username);
+//         System.out.println("Domain: " + domain);
+//         System.out.println("UName: " + UName);
+//         System.out.println("Dnamee: " + Dnamee);
+//         System.out.println("DName: " + DName);
+//     }
+// }
+// In this example, we extract the username and domain from an email address using the substring method.
+
+// More SUbstring Examples:
+// public class Learning_6 {
+//     public static void main(String[] args) {
+//         String str = "Hello, World!";
+        
+//         // Extracting "Hello"
+//         String hello = str.substring(0, 5);
+//         System.out.println(hello); // Output: Hello
+        
+//         // Extracting "World"
+//         String world = str.substring(7, 12);
+//         System.out.println(world); // Output: World
+        
+//         // Extracting from index 7 to the end
+//         String fromIndex7 = str.substring(7);
+//         System.out.println(fromIndex7); // Output: World!
+//     }
+// }
+
+
+
+
