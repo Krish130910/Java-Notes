59f9aa8b68180135a6f03f3bb764872250a9db42
Krish <krish.savaliya130910@marwadiuniversity.ac.in>
2026-02-04 15:45:00 +0530

    oop basics

---
+++ L11.java
@@ -0,0 +1,37 @@
+// JAVA NOTES
+// Object-Oriented Programming (OOP) Concepts, Constructor
+
+// OOP Concepts\
+// Object-Oriented Programming (OOP) is a programming paradigm that uses "objects" to represent data and methods to manipulate that data.
+// Object is a reference data type
+// Class is a blueprint for creating objects
+
+// Example of a simple class and object in Java
+// class Car {
+//     String color; // Variable to store the color of the car
+//     String model; // Variable to store the model of the car
+//     int year; // Variable to store the year of the car
+
+//     methods can also be defined here
+//     void startEngine() {
+//         System.out.println("Engine started");
+//     }
+// }
+// public class L11 {
+//     public static void main(String[] args) {
+//         Car car = new Car(); // Creating an object of the Car class
+//         car.color = "Red"; // Setting the color of the car object
+//         car.model = "Toyota"; // Setting the model of the car object
+//         car.year = 2020; // Setting the year of the car object
+//         System.out.println("Car Color: " + car.color); // Accessing the color of the car object
+//         System.out.println("Car Model: " + car.model); // Accessing the model of the car object
+//         System.out.println("Car Year: " + car.year); // Accessing the year of the car object
+//         car.startEngine(); // Calling the method to start the engine
+//     }
+// }
+// the above code demonstrates the basic concepts of Object-Oriented Programming (OOP) in Java by defining a class and creating an object of that class.                    
+
+// Constructor
+// A constructor is a special method that is called when an object is instantiated. It is used to initialize the object's properties.
+// A constructor has the same name as the class and does not have a return type.
+
