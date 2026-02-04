bdb7a0ce620e72400105b837ea1154ab887748ce
Krish <krish.savaliya130910@marwadiuniversity.ac.in>
2026-02-04 15:51:02 +0530

    constructors

--- L11.java
+++ L11.java
@@ -34,4 +34,34 @@
 // Constructor
 // A constructor is a special method that is called when an object is instantiated. It is used to initialize the object's properties.
 // A constructor has the same name as the class and does not have a return type.
+// to give unique values to different objects of the same class, we use constructors
+// Syntax of Constructor:
+// class ClassName {
+//     // properties
+//    ClassName(parameters) {
+//         // initialization code
+//     }
+// }
 
+// public class Student {
+//     String name;
+//     int age;
+//     double grade;
+//     boolean isPassed;
+//     // Constructor to initialize the properties of the Student class
+//     Student(String name, int age, double grade, boolean isPassed) {
+//         this.name = name; // 'this' keyword is used to refer to the current object's properties
+//         this.age = age;
+//         this.grade = grade;
+//         this.isPassed = grade >= 50; // Determine if the student has passed based on the grade
+//     }
+//     public static void main(String[] args) {
+//         // Creating objects of the Student class using the constructor
+//         Student student1 = new Student("Alice", 20, 85.5, true);
+//         Student student2 = new Student("Bob", 22, 45.0, false);
+//         // Displaying the properties of the student objects
+//         System.out.println("Student 1: " + student1.name + ", Age: " + student1.age + ", Grade: " + student1.grade + ", Passed: " + student1.isPassed); 
+//         System.out.println("Student 2: " + student2.name + ", Age: " + student2.age + ", Grade: " + student2.grade + ", Passed: " + student2.isPassed);
+//     }
+// }
+// the above code demonstrates the use of constructors in Java to initialize the properties of objects when they are created.
